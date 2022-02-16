6a8fd3009eb24eda84f6f75e1815d732fabfc36f
negar maleki <negar.mlk93@yahoo.com>
2022-02-16 12:50:34 +0330

    add comment added

--- login.php
+++ login.php
@@ -3,6 +3,7 @@ $db=mysql_connect('localhost','root','');
 mysqli_select_db($db,'db1');
 mysqli_query($db,"SET NAMES 'UTF8'");
 // fix bug
+// fix
 $username=htmlentities(addslashes($_POST['username'];),ENT_QUOTES,'UTF8');
 $password=htmlentities(addslashes($_POST['password'];),ENT_QUOTES,'UTF8');
 $q="SELECT * FROM users WHERE username = '$username' 
